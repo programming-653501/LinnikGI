e32e443a89f0cd61f30bc4b0ce50ed13138bbe46
Gleb Linnik <gleb_linnik@icloud.com>
2017-03-24 15:04:50 +0300

    4 laba fist task ready

---
+++ Laba4/4.1.15.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct WordsArray {
+    int count;
+    char **wordsArr;
+} WordsArray;
+WordsArray *getWordsFromString(char *, const char *);
+int deleteCharAtString(char *, int);
+
+int main() {
+    char *text = (char *)calloc(1024, sizeof(char));
+    char *input = (char *)calloc(100, sizeof(char));
+
+    while (scanf("%[^\n]%*c", input) == 1) {
+        strcat(text, input);
+        strcat(text, " ");
+    }
+
+    WordsArray *words = getWordsFromString(text, " ");
+    //Editing every word
+    for (int i = 0; i < words->count; ++i) {
+        char *currentWord = words->wordsArr[i];
+        int length = (int)strlen(currentWord);
+        //First editing (Swapping the first ans the last character)
+        char firstChar = currentWord[0];
+        deleteCharAtString(currentWord, 0);
+        currentWord[length - 1] = firstChar;
+        printf("%s\n", currentWord);
+        //Second editing (Deleting all repeating characters)
+        for (int j = 0; j < length; ++j) {
+            char currentCharacter = currentWord[j];
+            for (int k = 0; k < length; ++k) {
+                if (currentCharacter == currentWord[k] && k != j)
+                    deleteCharAtString(currentWord, k);
+            }
+        }
+    }
+
+
+    printf("%s\n", words->wordsArr[0]);
+    //>Here goes memory deallocation code< doyebatsa zdes
+    return 0;
+}
+
+WordsArray *getWordsFromString(char *string, const char *separator) {
+    char **words = (char **)malloc(sizeof(char *));
+    int allocatedMemory = 1;
+    int wordsCount = 0;
+
+    const char *token = strtok(string, separator);
+    for (; token; wordsCount++) {
+        if (allocatedMemory < wordsCount + 1) {
+            allocatedMemory *= 2;
+            words = (char **)realloc(words, sizeof(char *) * allocatedMemory);
+        }
+        words[wordsCount] = (char *)malloc(sizeof(char) * strlen(token));
+        strcpy(words[wordsCount], token);
+        token = strtok(NULL, separator);
+    }
+    WordsArray *tempWords = (WordsArray *)malloc(sizeof(WordsArray));
+    tempWords->count = wordsCount;
+    tempWords->wordsArr = words;
+    return tempWords;
+}
+
+int deleteCharAtString(char *string, int index) {
+    int length = (int)strlen(string);
+
+    if (index > length - 1)
+        return 1;
+
+    memmove(string + index,
+            string + (index + 1),
+            sizeof(char) * length);
+    return 0;
+}
---
+++ Laba4/cmake-build-debug/CMakeFiles/Laba4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba4.dir/4.1.15.c.o"
+  "Laba4.pdb"
+  "Laba4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Laba4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

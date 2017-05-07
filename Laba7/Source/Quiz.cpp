a15b165811f73a7490f64acbb709c24f152d7e9c
Gleb Linnik <gleb_linnik@icloud.com>
2017-05-07 19:25:25 +0300

    input check added

--- Laba7/Source/Quiz.cpp
+++ Laba7/Source/Quiz.cpp
@@ -5,6 +5,10 @@
 #include "../Header/Quiz.h"
 #include <iostream>
 
+void clearScreen() {
+    std::cout << std::string(100, '\n');
+}
+
 Quiz::Quiz(const json &quizJson) : quizJson(quizJson) {
     name = quizJson["name"].get<std::string>();
     for (json &topic : this->quizJson["topics"]) {
@@ -29,33 +33,42 @@ void Quiz::start() {
         for (int i = 0; i < topics.size(); ++i) {
             std::cout << i + 1 << ") " << topics[i].title << std::endl;
         }
-        std::cout << "0) Exit\n";
+        std::cout << "\n0) Exit\n";
         std::cout << "Choose the topic: ";
         int choice;
         std::cin >> choice;
-        if (choice) {
-            if (choice <= topics.size())
-                startQuizWithTopic(topics[choice - 1]);
+        if(std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            clearScreen();
+            std::cout << "Please, enter correct input.\n\n";
         } else {
-            break;
+            if (choice) {
+                if (choice <= topics.size())
+                    startQuizWithTopic(topics[choice - 1]);
+            } else {
+                break;
+            }
         }
     }
 }
 
 void Quiz::startQuizWithTopic(const Topic &topic) {
     uint correntAnswers = 0;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     for (int i = 0; i < topic.questions.size(); ++i) {
         Question question = topic.questions[i];
         std::cout << i + 1 << " question:\n" << question.question << std::endl;
         std::cout << "Answer: ";
         std::string answer;
-        std::cin >> answer;
+        std::getline(std::cin, answer);
+        clearScreen();
         if (std::find(question.answers.begin(), question.answers.end(), answer) != question.answers.end()) {
-            std::cout << "Correct!\n";
             correntAnswers++;
+            std::cout << "Correct!\n\n";
         } else {
-            std::cout << "You just suck!\n";
+            std::cout << "You just suck!\n\n";
         }
     }
-    std::cout << "You got " << correntAnswers << " out of " << topic.questions.size() << std::endl;
+    std::cout << "You got " << correntAnswers << " out of " << topic.questions.size() << "\n\n";
 }
